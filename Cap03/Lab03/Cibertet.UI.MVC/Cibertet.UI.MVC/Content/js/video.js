c0e216d7eb1ff38fdc2ef6608fab0b579415cb96
victoryume <victor.yume4@gmail.com>
2018-12-30 13:39:32 -0500

    3.3

--- Cap03/Lab03/Cibertet.UI.MVC/Cibertet.UI.MVC/Content/js/video.js
+++ Cap03/Lab03/Cibertet.UI.MVC/Cibertet.UI.MVC/Content/js/video.js
@@ -7,6 +7,20 @@
             var videoElement = document.getElementById(videoId);
 
             videoElement.play();
+        },
+
+        pause: function (videoId) {
+
+            var videoElement = document.getElementById(videoId);
+
+            videoElement.pause();
+        },
+
+        stop: function (videoId) {
+
+            var videoElement = document.getElementById(videoId);
+            videoElement.currentTime = 0;
+            videoElement.pause();
         }
     }
 })(window.cibertec = window.cibertec || {});
