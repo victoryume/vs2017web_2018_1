5480092ed8cede1ea0314a7794e44ea3f8fa0ecc
victoryume <victor.yume4@gmail.com>
2019-01-13 13:23:39 -0500

    ll

---
+++ Cap04/Lab02/Lab02Templates/Lab02Templates/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab02Templates.Startup))]
+namespace Lab02Templates
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
