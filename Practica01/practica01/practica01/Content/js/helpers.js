5430e2e125caf1bf61348d9641e59e1e09c3d4a2
victoryume <38994722+victoryume@users.noreply.github.com>
2019-01-25 19:06:54 -0500

    --

---
+++ Practica01/practica01/practica01/Content/js/helpers.js
@@ -0,0 +1,49 @@
+﻿
+(function (cibertec) {
+
+    cibertec.helpers = {
+
+        
+        SetObjetctLocalStorage: function (jsObject, keyStorage) {
+            //stringify: SERIALIZA
+            var objectString = JSON.stringify(jsObject);
+            //ALMACENAR
+            localStorage.setItem(keyStorage, objectString);
+
+        },
+
+        GetObjetctLocalStorage: function (keyStorage) {
+            //parse: DESERIALIZA
+            var objectString = localStorage.getItem(keyStorage);
+            var objectJS = JSON.parse(objectString);
+            
+            return objectJS;
+
+		},
+
+		soloNumeros: function (e) {
+			key = e.keyCode || e.which;
+			teclado = String.fromCharCode(key).toLowerCase();
+			numeros = ".1234567890";
+			especiales = "8-37-38-46"; // Teclas especiales para ejecutarse
+			/*
+			8: backspace
+			37: tecla izquierda
+			38: tecla derecha
+			46: suprimir
+			*/
+			teclado_epecial = false;
+
+			for (var i in especiales) {
+				if (key == especiales[i]) {//si el key es igual a los especiale spermitidos
+					teclado_epecial = true; break;
+				}
+			}
+
+			if (numeros.indexOf(teclado) == -1 && !teclado_epecial) {
+				return false; //no aceptará el caracter(no dejará escribir)
+			}
+		}
+               
+    }
+})(window.cibertec = window.cibertec || {});
---
+++ Practica01/practica01/practica01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="practica01.MvcApplication" Language="C#" %>
