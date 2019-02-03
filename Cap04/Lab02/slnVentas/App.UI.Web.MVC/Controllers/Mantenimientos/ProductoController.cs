6a98c9b0767ef69d68a800da8b4d9dfd81840dba
victoryume <victor.yume4@gmail.com>
2019-02-03 13:10:30 -0500

    ..

--- Cap04/Lab02/slnVentas/App.UI.Web.MVC/Controllers/Mantenimientos/ProductoController.cs
+++ Cap04/Lab02/slnVentas/App.UI.Web.MVC/Controllers/Mantenimientos/ProductoController.cs
@@ -38,13 +38,20 @@ namespace App.UI.Web.MVC.Controllers.Mantenimientos
         public ActionResult Index2(string filterByName, int? filterByCategoria, int? filterByMarca)
         {
             filterByName = string.IsNullOrWhiteSpace(filterByName) ? "" : filterByName.Trim();
-            ViewBag.filterByName = filterByName;
+            //ViewBag.filterByName = filterByName;
             ViewBag.Categorias = categoriaService.GetAll("");
             ViewBag.Marcas = marcaService.GetAll("");
+            //var model = productoService.GetAll(filterByName, filterByCategoria, filterByMarca);
+            return View();
+        }
 
+        public ActionResult Index2Buscar(string filterByName, int? filterByCategoria, int? filterByMarca)
+        {
+            filterByName = string.IsNullOrWhiteSpace(filterByName) ? "" : filterByName.Trim();
 
             var model = productoService.GetAll(filterByName, filterByCategoria, filterByMarca);
-            return View(model);
+            
+            return PartialView("Index2Resultado", model);
         }
     }
 }
