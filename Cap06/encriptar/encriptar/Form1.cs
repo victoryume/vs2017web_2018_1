be799c2dc1d0ec261f92e2f39dbfbffb124551bc
victoryume <victor.yume4@gmail.com>
2019-02-24 13:49:03 -0500

    .

---
+++ Cap06/encriptar/encriptar/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace encriptar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = "123456";
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, 20))
+            {
+                byte[] saltos = deriveBytes.Salt;
+                byte[] key = deriveBytes.GetBytes(20);
+
+            }
+        }
+    }
+}
